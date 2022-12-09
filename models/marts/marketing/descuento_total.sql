c25c33d6063b581888a47ac0199501bb9e837ca0
jmpuertollanosuarez@gmail.com <jmpuertollanosuarez@gmail.com>
2022-12-09 18:33:38 +0000

    código nuevo - 09/12/2022

---
+++ models/marts/marketing/descuento_total.sql
@@ -0,0 +1,22 @@
+WITH dim_users AS (
+    SELECT * 
+    FROM {{ ref('dim_users') }}
+    ),
+
+    orders as ( SELECT * 
+    FROM {{ ref('fct_orders') }}
+    ),
+
+renamed_casted AS (
+    SELECT
+          dim_users.user_id
+        , first_name
+        , last_name
+        , SUM(orders.order_cost + orders.shipping_cost - orders.order_total) :: decimal(20,2) AS total_descuento
+    FROM dim_users dim_users join orders orders
+        on dim_users.user_id = orders.user_id
+    group by dim_users.user_id, dim_users.first_name, dim_users.last_name
+    order by 4 desc
+    )
+
+SELECT * FROM renamed_casted
